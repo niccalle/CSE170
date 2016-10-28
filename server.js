6718f7e596981dd701bc3cc82db3793b211f715d
Nicolas Calle <ncalle@ucsd.edu>
2016-10-27 22:39:20 -0700

    Added a procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express")
 var app = express()
 var config = require("./server/config");
 app = config.initialize(app);
-
+app.set('port', (process.env.PORT || 5000));
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -49,6 +49,6 @@ app.get("/workout/:workout/:playlist/begin", function(req,res){
 app.get("/getSongs/:playlist", function(req,res){
   res.send({songs: ["270890617","210883449"]});
 })
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+app.listen(app.get('port'), function(){
+  console.log("Listening on port "+app.get('port'));
 })
