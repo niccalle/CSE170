766beaf65cd2a44e59ccf285ee254d544974941a
Victor <victorc1165@yahoo.com>
2016-11-26 00:16:40 -0800

    google analytics set up for calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1052,6 +1052,8 @@ if(!String.prototype.formatNum) {
 			event.preventDefault();
 			event.stopPropagation();
 
+			//console.log("HI");
+
 			var url = $(this).attr('href');
 			var id = $(this).data("event-id");
 			var event = _.find(self.options.events, function(event) {
@@ -1103,8 +1105,19 @@ if(!String.prototype.formatNum) {
 					})
 					.data('handled.bootstrap-calendar', true).data('handled.event-id', event.id);
 			}
-			console.log(url);
-			console.log($("modal-link"));
+			// console.log(url);
+			// console.log($("modal-link"));
+
+			// analytics code
+		    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		    ga('create', 'UA-87335227-1', 'auto');
+		    ga('send', 'event', 'button', 'click');
+		    //console.log("TRaked");
+
 			$("#modal-link")[0].href=url;
 			modal.modal('show');
 		});
